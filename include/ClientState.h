7ac08c445813f707dc54f002bef46b121dabac63
Lion Kortlepel <development@kortlepel.com>
2024-03-02 16:34:44 +0100

    add client state

---
+++ include/ClientState.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace bmp {
+
+enum ClientState {
+    ClientIdentification = 0xC0,
+    Login = 0xC1,
+    QuickJoin = 0xC2,
+    Browsing = 0xC3,
+    ServerIdentification = 0xC4,
+    ServerAuthentication = 0xC5,
+    ServerModDownload = 0xC6,
+    ServerSessioSetup = 0xC7,
+    ServerPlaying = 0xC8, // marked as TODO in the spec?!
+    ServerLeaving = 0xC9, // marked as TODO in the spec?!
+};
+
+}
