fbed4988a3ecf02c60609a6a966ddf095bc5a641
Lion Kortlepel <development@kortlepel.com>
2024-03-02 16:58:07 +0100

    fix typo in client state

--- include/ClientState.h
+++ include/ClientState.h
@@ -10,7 +10,7 @@ enum ClientState {
     ServerIdentification = 0xC4,
     ServerAuthentication = 0xC5,
     ServerModDownload = 0xC6,
-    ServerSessioSetup = 0xC7,
+    ServerSessionSetup = 0xC7,
     ServerPlaying = 0xC8, // marked as TODO in the spec?!
     ServerLeaving = 0xC9, // marked as TODO in the spec?!
 };
